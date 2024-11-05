5d7894a0c9868ebfbacbc4728d95cfb332d27828
fpluske <frantisek.pluske@seznam.cz>
2024-11-05 13:13:10 +0100

    V 0.19

--- main.js
+++ main.js
@@ -1,4 +1,11 @@
 const statistika = document.getElementById('Statistika');
-const imgKostka = document.getElementsByTagName('img');
+const imgKostka = document.getElementsByTagName('img')[0];
 const infoKostka = document.querySelector('.badge.bg-dark');
-console.log(imgKostka);
+const btnHrej = document.querySelector('button');
+let hod;
+
+btnHrej.addEventListener('click', function() {
+    hod = Math.floor(Math.random()*6)+1;
+    infoKostka.innerHTML = hod;
+    imgKostka.src = `./img/kostka${hod}.png`;
+});
