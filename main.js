091d8b683ab4c9644d5545958d98d61664471988
fpluske <frantisek.pluske@seznam.cz>
2024-11-05 12:48:17 +0100

    V 0.11

---
+++ main.js
@@ -0,0 +1,4 @@
+const statistika = document.getElementById('Statistika');
+const imgKostka = document.getElementsByTagName('img');
+const infoKostka = document.querySelector('.badge.bg-dark');
+console.log(imgKostka);
